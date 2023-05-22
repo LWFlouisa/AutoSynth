dec50fff8699431c58152f8213de03661cfa36d0
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2023-05-22 10:14:13 -0500

    Update adaptational_vortex.rb

--- adaptational_vortex.rb
+++ adaptational_vortex.rb
@@ -1,151 +1,233 @@
-def create_rulesets
-  row = 0 ## The value of row is 0.
+require 'humanist_errors'
+include HumanistErrors
 
-  ## The directory knwon_ruleset is _ruleset/rules.txt
-  File.readlines("_ruleset/rules.txt")
+with_human_errors do
 
-  ### Values of the individual rows, as Ruby counts from zero.
-  rule_1 = row + 1
-  rule_2 = row + 2
-  rule_3 = row + 3
-  rule_4 = row + 4
-  rule_5 = row + 5
-  rule_6 = row + 6
-  rule_7 = row + 7
-  rule_8 = row + 8
-  rule_9 = row + 9
-
-  ### Creation of the actual rulesets.
-  ruleset_1 = "#{rule1}
-#{rule2}
-#{rule3}
-#{rule4}
-#{rule5}
-#{rule6}
-#{rule7}
-#{rule8}
-#{rule9}"
-
-  ruleset_2 = "#{rule9}
-#{rule1}
-#{rule2}
-#{rule3}
-#{rule4}
-#{rule5}
-#{rule6}
-#{rule7}
-#{rule8}"
-
-  ruleset_3 = "#{rule8}
-#{rule9}
-#{rule1}
-#{rule2}
-#{rule3}
-#{rule4}
-#{rule5}
-#{rule6}
-#{rule7}"
-
-  ruleset_4 = "#{rule7}
-#{rule8}
-#{rule9}
-#{rule1}
-#{rule2}
-#{rule3}
-#{rule4}
-#{rule5}
-#{rule6}"
-
-  ruleset_5 = "#{rule6}
-#{rule7}
-#{rule8}
-#{rule9}
-#{rule1}
-#{rule2}
-#{rule3}
-#{rule4}
-#{rule5}"
-
-  ruleset_6 = "#{rule5}
-#{rule6}
-#{rule7}
-#{rule8}
-#{rule9}
-#{rule1}
-#{rule2}
-#{rule3}
-#{rule4}"
-
-  ruleset_7 = "#{rule4}
-#{rule5}
-#{rule6}
-#{rule7}
-#{rule8}
-#{rule9}
-#{rule1}
-#{rule2}
-#{rule3}"
-
-  ruleset_8 = "#{rule3}
-#{rule4}
-#{rule5}
-#{rule6}
-#{rule7}
-#{rule8}
-#{rule9}
-#{rule1}
-#{rule2}"
-
-  ruleset_9 = "#{rule2}
-#{rule3}
-#{rule4}
-#{rule5}
-#{rule6}
-#{rule7}
-#{rule8}
-#{rule9}
-#{rule1}"
-
-  open("_adaptation/ruleset_shift1.txt", "w") { |f|
-    f.puts ruleset_1
-  }
-
-  open("_adaptation/ruleset_shift2.txt", "w") { |f|
-    f.puts ruleset_2
-  }
-
-  open("_adaptation/ruleset_shift3.txt", "w") { |f|
-    f.puts ruleset_3
-  }
-
-  open("_adaptation/ruleset_shift4.txt", "w") { |f|
-    f.puts ruleset_4
-  }
-
-  open("_adaptation/ruleset_shift5.txt", "w") { |f|
-    f.puts ruleset_5
-  }
-
-  open("_adaptation/ruleset_shift6.txt", "w") { |f|
-    f.puts ruleset_6
-  }
-
-  open("_adaptation/ruleset_shift7.txt", "w") { |f|
-    f.puts ruleset_7
-  }
-
-  open("_adaptation/ruleset_shift8.txt", "w") { |f|
-    f.puts ruleset_8
-  }
-
-  open("_adaptation/ruleset_shift9.txt", "w") { |f|
-    f.puts ruleset_9
-  }
-end
+#def create_rulesets
+#  row = 0 ## The value of row is 0.
+#
+#  ## The directory knwon_ruleset is _ruleset/rules.txt
+#  ruleset = File.readlines("_rulesets/rules.txt")
+#
+#  ### Values of the individual rows, as Ruby counts from zero.
+#  rule1 = 0 #row + 1
+#  rule2 = 1 #row + 2
+#  rule3 = 2 #row + 3
+#  rule4 = 3 #row + 4
+#  rule5 = 4 #row + 5
+#  rule6 = 5 #row + 6
+#  rule7 = 6 #row + 7
+#  rule8 = 7 #row + 8
+#  rule9 = 8 #row + 9
+#
+#  ### Creation of the actual rulesets.
+#  ruleset_1 = "#{ruleset[rule1].tr '
+#', ''}
+#{ruleset[rule2].tr '
+#', ''}
+#{ruleset[rule3].tr '
+#', ''}
+#{ruleset[rule4].tr '
+#', ''}
+#{ruleset[rule5].tr '
+#', ''}
+#{ruleset[rule6].tr '
+#', ''}
+#{ruleset[rule7].tr '
+#', ''}
+#{ruleset[rule8].tr '
+#', ''}
+#{ruleset[rule9]}"
+#
+#  ruleset_2 = "#{ruleset[rule9]}
+#{ruleset[rule1].tr '
+#', ''}
+#{ruleset[rule2].tr '
+#', ''}
+#{ruleset[rule3].tr '
+#', ''}
+#{ruleset[rule4].tr '
+#', ''}
+#{ruleset[rule5].tr '
+#', ''}
+#{ruleset[rule6].tr '
+#', ''}
+#{ruleset[rule7].tr '
+#', ''}
+#{ruleset[rule8].tr '
+#', ''}"
+#
+#  ruleset_3 = "#{ruleset[rule8].tr '
+#', ''}
+#{ruleset[rule9]}
+#{ruleset[rule1].tr '
+#', ''}
+#{ruleset[rule2].tr '
+#', ''}
+#{ruleset[rule3].tr '
+#', ''}
+#{ruleset[rule4].tr '
+#', ''}
+#{ruleset[rule5].tr '
+#', ''}
+#{ruleset[rule6].tr '
+#', ''}
+#{ruleset[rule7].tr '
+#', ''}"
+#
+#  ruleset_4 = "#{ruleset[rule7].tr '
+#', ''}
+#{ruleset[rule8].tr '
+#', ''}
+#{ruleset[rule9]}
+#{ruleset[rule1].tr '
+#', ''}
+#{ruleset[rule2].tr '
+#', ''}
+#{ruleset[rule3].tr '
+#', ''}
+#{ruleset[rule4].tr '
+#', ''}
+#{ruleset[rule5].tr '
+#', ''}
+#{ruleset[rule6].tr '
+#', ''}"
+#
+#  ruleset_5 = "#{ruleset[rule6].tr '
+#', ''}
+#{ruleset[rule7].tr '
+#', ''}
+#{ruleset[rule8].tr '
+#', ''}
+#{ruleset[rule9]}
+#{ruleset[rule1].tr '
+#', ''}
+#{ruleset[rule2].tr '
+#', ''}
+#{ruleset[rule3].tr '
+#', ''}
+#{ruleset[rule4].tr '
+#', ''}
+#{ruleset[rule5].tr '
+#', ''}"
+#
+#  ruleset_6 = "#{ruleset[rule5].tr '
+#', ''}
+#{ruleset[rule6].tr '
+#', ''}
+#{ruleset[rule7].tr '
+#', ''}
+#{ruleset[rule8].tr '
+#', ''}
+#{ruleset[rule9]}
+#{ruleset[rule1].tr '
+#', ''}
+#{ruleset[rule2].tr '
+#', ''}
+#{ruleset[rule3].tr '
+#', ''}
+#{ruleset[rule4].tr '
+#', ''}"
+#
+#  ruleset_7 = "#{ruleset[rule4].tr '
+#', ''}
+#{ruleset[rule5].tr '
+#', ''}
+#{ruleset[rule6].tr '
+#', ''}
+#{ruleset[rule7].tr '
+#', ''}
+#{ruleset[rule8].tr '
+#', ''}
+#{ruleset[rule9]}
+#{ruleset[rule1].tr '
+#', ''}
+#{ruleset[rule2].tr '
+#', ''}
+#{ruleset[rule3].tr '
+#', ''}"
+#
+#  ruleset_8 = "#{ruleset[rule3].tr '
+#', ''}
+#{ruleset[rule4].tr '
+#', ''}
+#{ruleset[rule5].tr '
+#', ''}
+#{ruleset[rule6].tr '
+#', ''}
+#{ruleset[rule7].tr '
+#', ''}
+#{ruleset[rule8].tr '
+#', ''}
+#{ruleset[rule9]}
+#{ruleset[rule1].tr '
+#', ''}
+#{ruleset[rule2].tr '
+#', ''}"
+#
+#  ruleset_9 = "#{ruleset[rule2].tr '
+#', ''}
+#{ruleset[rule3].tr '
+#', ''}
+#{ruleset[rule4].tr '
+#', ''}
+#{ruleset[rule5].tr '
+#', ''}
+#{ruleset[rule6].tr '
+#', ''}
+#{ruleset[rule7].tr '
+#', ''}
+#{ruleset[rule8].tr '
+#', ''}
+#{ruleset[rule9]}
+#{ruleset[rule1]}.tr '
+#', ''"
+#
+#  open("_adaptation/ruleset_shift1.txt", "w") { |f|
+#    f.puts ruleset_1
+#  }
+#
+#  open("_adaptation/ruleset_shift2.txt", "w") { |f|
+#    f.puts ruleset_2
+#  }
+#
+#  open("_adaptation/ruleset_shift3.txt", "w") { |f|
+#    f.puts ruleset_3
+#  }
+#
+#  open("_adaptation/ruleset_shift4.txt", "w") { |f|
+#    f.puts ruleset_4
+#  }
+#
+#  open("_adaptation/ruleset_shift5.txt", "w") { |f|
+#    f.puts ruleset_5
+#  }
+#
+#  open("_adaptation/ruleset_shift6.txt", "w") { |f|
+#    f.puts ruleset_6
+#  }
+#
+#  open("_adaptation/ruleset_shift7.txt", "w") { |f|
+#    f.puts ruleset_7
+#  }
+#
+#  open("_adaptation/ruleset_shift8.txt", "w") { |f|
+#    f.puts ruleset_8
+#  }
+#
+#  open("_adaptation/ruleset_shift9.txt", "w") { |f|
+#    f.puts ruleset_9
+#  }
+#end
 
 def adaptation_vortex
   ## Create new sound_file with new ruleset permutation.
   ruleset_choice = File.read("_rulesets/choice/value.txt").to_s.to_i
+
+  # Resets reset choice if at 8 in Ruby.
+  if ruleset_choice > 8
+    ruleset_choice = 0
+  end
 
   mutation1 = File.readlines("_adaptation/ruleset_shift1.txt")
   mutation2 = File.readlines("_adaptation/ruleset_shift2.txt")
@@ -182,8 +264,6 @@ def adaptation_vortex
   system("#{chosen_ruleset[vortex_row_5]}")
   system("#{chosen_ruleset[vortex_row_1]}")
 
-  ruleset_choice = ruleset_choice + 1
-
   open("_rulesets/choice/value.txt", "w") { |f|
     ruleset_choice = ruleset_choice + 1
 
@@ -192,11 +272,15 @@ def adaptation_vortex
 end
 
 ## Only create new rulesets if set to true.
-rule_creation = true
+#rule_creation = false
 
-if rule_creation == true
-  create_rulesets
-  adaptation_vortex
-else
-  adaptation_vortex
+#if rule_creation == true
+  #create_rulesets
+  # adaptation_vortex
+#else
+  #adaptation_vortex
+#end
+
+adaptation_vortex
+
 end
